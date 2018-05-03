752d30dc99bf7837549bb953e9c217fd1ee8fe4a
ku20298 <31235499+ku20298@users.noreply.github.com>
2018-05-03 17:48:34 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"strconv"
 	"golang.org/x/image/colornames"
 	"github.com/hajimehoshi/ebiten/text"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/inpututil"
 	"golang.org/x/image/font"
+	"github.com/ku20/Ebiten/localstorage"
 	
+	"strconv"
 	"math/rand"
 	"log"
 	"time"
@@ -17,58 +18,55 @@ const screenWidth = 360
 const screenHeight = 640
 
 var mainFont font.Face
+var subFont font.Face
 var rensyo int
-var renpai int
 var win bool
-var lose bool
-var winCount float64
-var loseCount float64
-var syoritsu string
+var winCount int
+var loseCount int
+var saikou int
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 
 	mainFont = decodeFont(fontByte, 56)
+	subFont = decodeFont(fontByte, 32)
 
 	rensyo = 0
-	renpai = 0
-	win = false
-	lose = false
 }
 
 func update(screen *ebiten.Image) error {
-	jsEvent()
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) || len(inpututil.JustPressedTouchIDs()) > 0 {
+	screen.Fill(colornames.White)
+	// jsEvent()
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) || len(inpututil.JustPressedTouches()) > 0 {
 		if rand.Intn(2) == 1 {
 			rensyo ++
-			lose = false			
 			win = true
 			winCount ++
 		}else {
+			if rensyo >= saikou {
+				saikou = rensyo
+			}
 			rensyo = 0
 			win = false
-			lose = true
 			loseCount ++
 		}
 	}
 
-	screen.Fill(colornames.White)
+	text.Draw(screen, "最高 " + strconv.Itoa(saikou), subFont, 134, 86, colornames.Black)
+	
 
 	text.Draw(screen, strconv.Itoa(rensyo), mainFont, 100, 200, colornames.Black)
 	text.Draw(screen, "連勝", mainFont, 160, 200, colornames.Black)
-	if win {
-		for i := 0; i < rensyo; i++ {
+
+	if winCount + loseCount == 0 {
+		text.Draw(screen, "タップで勝負!", subFont, 82, 340, colornames.Black)
+	}else {
+		if win {
 			text.Draw(screen, "勝ち", mainFont, 124, 340, colornames.Red)
+		}else {
+			text.Draw(screen, "負け", mainFont, 124, 340, colornames.Blue)
 		}
 	}
-	if lose {
-		text.Draw(screen, "負け", mainFont, 124, 340, colornames.Blue)
-	}
-
-	if winCount + loseCount > 0 {
-		syoritsu = "勝率:" + strconv.Itoa(int(winCount / (winCount + loseCount) * 100)) + "%"
-	} 
-	text.Draw(screen, syoritsu, mainFont, 70, 500, colornames.Black)
 	
 	return nil
 }
